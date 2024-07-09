18865d29f88c52723d3bcdcc3aefdc03bcc7fafb
morgonic <167047845+morgonic@users.noreply.github.com>
2024-07-09 17:55:45 -0400

    Update links.js

--- js/links.js
+++ js/links.js
@@ -2,7 +2,7 @@ export const links = [
 	{ 
 		name: "Create E-Mail for reporting The Heritage Foundation.", 
 		link: 
-"mailto:eoclass@irs.gov?subject=13909%20for%20The%20Heritage%20Foundation&body=I%20have%20attached%20the%2013909%20form%20in%20order%20to%20report%20The%20Heritage%20Foundation%20for%20falsely%20claiming%20tax%20exemption%20status.", 
+"mailto::eoclass@irs.gov?subject=Form%2013909%20Tax-Exempt%20Violation%20The%20Heritage%20Foundation&body=Attached%20is%20a%20completed%20Form%2013909,%20which%20details%20The%20Heritage%20Foundation's%20violations%20%of%20Internal%20Revenue%20%Code%501(c)",
 		image: 
 "https://cdn4.iconfinder.com/data/icons/mail-1-1/1024/report_mail-512.png", 
 	}
