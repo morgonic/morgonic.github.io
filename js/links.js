ab70cd30f981b909322c98fd8aeb4416a42f24ee
morgonic <167047845+morgonic@users.noreply.github.com>
2024-07-09 21:56:22 -0400

    Update links.js

--- js/links.js
+++ js/links.js
@@ -6,18 +6,18 @@ export const links = [
 		image: 
 "https://cdn4.iconfinder.com/data/icons/mail-1-1/1024/report_mail-512.png", 
 	},
-	{ 
-		name: "E-Mail Template Example",
-		link:
-"https://docs.google.com/document/d/18sfyEH-CYqI6RY0GDdWZ1o6lYX-cFg212xzIPVCD6tk/edit",
-		image: 
-"https://cdn4.iconfinder.com/data/icons/mail-1-1/1024/report_mail-512.png", 
-	},
 	{
 		name: "Create E-Mail To Report AIPAC Directly To IRS",
 		link:
 "mailto:eoclass@irs.gov?subject=Form%2013909%20Tax-Exempt%20Violation%20AIPAC&body=Attached%20is%20a%20completed%20Form%2013909,%20which%20details%20AIPAC's%20tax-exempt%20status%20violations%20per%20Internal%20Revenue%20Code%20501(c)(4).",
 		image:
 "https://cdn4.iconfinder.com/data/icons/mail-1-1/1024/report_mail-512.png", 	
+	},
+	{ 
+		name: "E-Mail Template Example",
+		link:
+"https://docs.google.com/document/d/18sfyEH-CYqI6RY0GDdWZ1o6lYX-cFg212xzIPVCD6tk/edit",
+		image: 
+"https://cdn4.iconfinder.com/data/icons/mail-1-1/1024/report_mail-512.png", 
 	}
 ];
