063e7baeee44967d3a33b7b3d0f5ce89683be072
morgonic <167047845+morgonic@users.noreply.github.com>
2024-07-09 20:19:14 -0400

    Update links.js

--- js/links.js
+++ js/links.js
@@ -5,5 +5,12 @@ export const links = [
 "mailto:eoclass@irs.gov?subject=Form%2013909%20Tax-Exempt%20Violation%20The%20Heritage%20Foundation&body=Attached%20is%20a%20completed%20Form%2013909,%20which%20details%20The%20Heritage%20Foundation's%20violations%20of%20Internal%20Revenue%20Code%20501(c)(3).",
 		image: 
 "https://cdn4.iconfinder.com/data/icons/mail-1-1/1024/report_mail-512.png", 
+	},
+	{ 
+		name: "E-Mail Template Example",
+		link:
+"https://docs.google.com/document/d/18sfyEH-CYqI6RY0GDdWZ1o6lYX-cFg212xzIPVCD6tk/edit",
+		image: 
+"https://cdn4.iconfinder.com/data/icons/mail-1-1/1024/report_mail-512.png", 
 	}
 ];
