a307a2545c93aecaae4ba79e3c94b36134e6a6d9
morgonic <167047845+morgonic@users.noreply.github.com>
2024-07-27 19:31:20 -0400

    Update links2.js

--- js/links2.js
+++ js/links2.js
@@ -5,15 +5,15 @@ export const links = [
 "https://www.vote.org/register-to-vote/", 
 		image: 
 "https://cdn.prod.website-files.com/611678a38604514a59d8ef3f/6315cb8f05389a749a5ffe46_Screenshot%202022-09-05%20at%2012.12.11.png", 
-	}
-  { 
+	},
+ 	{ 
 		name: "CHECK YOUR REGISTRATION", 
 		link: 
 "https://www.vote.org/am-i-registered-to-vote/", 
 		image: 
 "https://cdn.prod.website-files.com/611678a38604514a59d8ef3f/6315cb8f05389a749a5ffe46_Screenshot%202022-09-05%20at%2012.12.11.png", 
-	}
-  { 
+	},
+	{ 
 		name: "WHAT'S ON THE BALLOT?", 
 		link: 
 "https://www.vote.org/ballot-information/", 
