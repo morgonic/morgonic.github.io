49b677e2bb91f5b5f717d9d9c26e87c2f780a8bd
morgonic <167047845+morgonic@users.noreply.github.com>
2024-07-27 21:05:24 -0400

    Create links6.js

---
+++ js/links6.js
@@ -0,0 +1,30 @@
+export const links = [ 
+	{ 
+		name: "SEED THE VOTE", 
+		link: 
+"https://www.mobilize.us/surj/event/652114/?referring_vol=6645280&rname=Morgon&timeslot=4436292&referring_participation=26283477&referring_data_signature=v1-807b82eb70cddbb1&share_medium=copy_link&share_context=signup-form-modal",
+		image: 
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0rduAtY6WMlc9Q18imBfATf0BuaO1lsUvNw&s", 
+	},
+	{
+		name: "PROJECT 2024: MAJORITY OVER MAGA",
+		link:
+"https://www.majorityovermaga.org/",
+		image:
+"https://indivisible.org/sites/default/files/2024-02/20240213-majority-over-maga-logo.png", 	
+	},
+	{ 
+		name: "THE SEVEN SEEDS INITIATIVE",
+		link:
+"https://moonflowermission.wordpress.com/2024/07/17/the-seven-seeds-initiative/",
+		image: 
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8KL6GRCKktEZAbUcoEEo4tkMsWPRei1dQQQ&s", 
+	},
+  { 
+		name: "STOP PROJECT 2025",
+		link:
+"https://moonflowermission.wordpress.com/2024/07/17/stop-project-2025/",
+		image: 
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8KL6GRCKktEZAbUcoEEo4tkMsWPRei1dQQQ&s", 
+	}
+];
