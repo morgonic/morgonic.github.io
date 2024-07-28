18282ef5358942f784a5f9ac3ed20a5230bfb9fd
morgonic <167047845+morgonic@users.noreply.github.com>
2024-07-27 20:29:10 -0400

    Create links4.js

---
+++ js/links4.js
@@ -0,0 +1,23 @@
+export const links = [ 
+	{ 
+		name: "CONTACT THE WHITE HOUSE ABOUT THE IRS", 
+		link: 
+"https://www.whitehouse.gov/contact/request/",
+		image: 
+"https://seeklogo.com/images/T/the-white-house-logo-5BCA6E4718-seeklogo.com.png", 
+	},
+	{
+		name: "WHITE HOUSE MESSAGE | PASTE INTO REQUEST",
+		link:
+"https://docs.google.com/document/d/1PEdGrHHDQ_iJG8iI2_373GW8h2dHaS39SNZQVsROu64/edit?usp=sharing",
+		image:
+"https://seeklogo.com/images/T/the-white-house-logo-5BCA6E4718-seeklogo.com.png", 	
+	},
+	{ 
+		name: "SHARE YOUR THOUGHTS WITH THE PRESIDENT",
+		link:
+"https://www.whitehouse.gov/contact/share/",
+		image: 
+"https://seeklogo.com/images/T/the-white-house-logo-5BCA6E4718-seeklogo.com.png", 
+	}
+];
