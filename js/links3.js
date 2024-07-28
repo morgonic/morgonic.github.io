57e256d9ab85c05ff475c5cb1d99bc01e586a989
morgonic <167047845+morgonic@users.noreply.github.com>
2024-07-27 21:00:20 -0400

    Update links3.js

--- js/links3.js
+++ js/links3.js
@@ -1,4 +1,11 @@
 export const links = [ 
+ 	{ 
+		name: "HERITAGE FOUNDATION COMPLETED FORM 13909", 
+		link: 
+"https://drive.google.com/file/d/11ox6BbRjSxkdgKd5Up3aSuJDHQLhuDh1/view?usp=sharing", 
+		image: 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
+	},
 	{ 
 		name: "ADVISORY BOARD COMPLETED FORMS 13909", 
 		link: 
@@ -6,10 +13,31 @@ export const links = [
 		image: 
 "https://www.irs.gov/pub/xml_bc/53478001.gif", 
 	},
- 	{ 
-		name: "HERITAGE FOUNDATION COMPLETED FORM 13909", 
+	{ 
+		name: "AIPAC COMPLETED FORM 13909", 
 		link: 
-"https://drive.google.com/file/d/11ox6BbRjSxkdgKd5Up3aSuJDHQLhuDh1/view?usp=sharing", 
+"https://drive.google.com/file/d/15fsrmcMd49OdMnmGB3K9r4gMyYx2Szqc/view", 
+		image: 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
+	},
+	{ 
+		name: "EMAIL TO IRS", 
+		link: 
+"https://morgonic.github.io/", 
+		image: 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
+	},
+	{ 
+		name: "MAIL TO IRS", 
+		link: 
+"https://drive.google.com/file/d/1GjYDxB-w_733GjzELVEBKtjk2QX62NXL/view?usp=drive_link", 
+		image: 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
+	},
+	{ 
+		name: "FILL OUT YOUR OWN FORM 13909", 
+		link: 
+"https://www.irs.gov/pub/irs-pdf/f13909.pdf", 
 		image: 
 "https://www.irs.gov/pub/xml_bc/53478001.gif", 
 	},
@@ -20,39 +48,11 @@ export const links = [
 		image: 
 "https://www.project2025.org/wp-content/uploads/2023/02/P2025-logo_full.png", 
 	},
-  	{ 
+	{ 
 		name: "ADVISORY BOARD ORGANIZATIONS ATTEMPT TO HIDE", 
 		link: 
 "https://theintercept.com/2024/07/17/project-2025-advisory-board/", 
 		image: 
 "https://www.project2025.org/wp-content/uploads/2023/02/P2025-logo_full.png", 
-	},
-  	{ 
-		name: "FILL OUT YOUR OWN FORM 13909", 
-		link: 
-"https://www.irs.gov/pub/irs-pdf/f13909.pdf", 
-		image: 
-"https://www.irs.gov/pub/xml_bc/53478001.gif", 
-	},
-  	{ 
-		name: "REPORT VIOLATORS TO IRS DIRECTLY", 
-		link: 
-"https://www.irs.gov/dmaf/form/13909", 
-		image: 
-"https://www.irs.gov/pub/xml_bc/53478001.gif", 
-	},
-  	{ 
-		name: "EMAIL TO IRS", 
-		link: 
-"https://morgonic.github.io/", 
-		image: 
-"https://www.irs.gov/pub/xml_bc/53478001.gif", 
-	},
-  	{ 
-		name: "MAIL TO IRS", 
-		link: 
-"https://drive.google.com/file/d/1GjYDxB-w_733GjzELVEBKtjk2QX62NXL/view?usp=drive_link", 
-		image: 
-"https://www.irs.gov/pub/xml_bc/53478001.gif", 
 	}
 ];
