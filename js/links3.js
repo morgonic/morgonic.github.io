59f6fe5f4243d49629dedb50c953bd708d1102ca
morgonic <167047845+morgonic@users.noreply.github.com>
2024-07-27 20:14:52 -0400

    Update links3.js

--- js/links3.js
+++ js/links3.js
@@ -4,14 +4,14 @@ export const links = [
 		link: 
 "https://docs.google.com/spreadsheets/d/13EMWDZw92TajWawsduam4Ctwa18CL7gn-OZAHL4uQQg/edit?gid=1132040679#gid=1132040679", 
 		image: 
-"https://www.project2025.org/wp-content/uploads/2023/02/P2025-logo_full.png", 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
 	},
  	{ 
 		name: "HERITAGE FOUNDATION COMPLETED FORM 13909", 
 		link: 
 "https://drive.google.com/file/d/11ox6BbRjSxkdgKd5Up3aSuJDHQLhuDh1/view?usp=sharing", 
 		image: 
-"https://www.project2025.org/wp-content/uploads/2023/02/P2025-logo_full.png", 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
 	},
 	{ 
 		name: "THE PEOPLE'S GUIDE TO PROJECT 2025", 
@@ -32,27 +32,27 @@ export const links = [
 		link: 
 "https://www.irs.gov/pub/irs-pdf/f13909.pdf", 
 		image: 
-"https://www.project2025.org/wp-content/uploads/2023/02/P2025-logo_full.png", 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
 	},
   	{ 
 		name: "REPORT VIOLATORS TO IRS DIRECTLY", 
 		link: 
 "https://www.irs.gov/dmaf/form/13909", 
 		image: 
-"https://www.project2025.org/wp-content/uploads/2023/02/P2025-logo_full.png", 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
 	},
   	{ 
 		name: "EMAIL TO IRS", 
 		link: 
 "https://morgonic.github.io/", 
 		image: 
-"https://www.project2025.org/wp-content/uploads/2023/02/P2025-logo_full.png", 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
 	},
   	{ 
 		name: "MAIL TO IRS", 
 		link: 
 "https://drive.google.com/file/d/1GjYDxB-w_733GjzELVEBKtjk2QX62NXL/view?usp=drive_link", 
 		image: 
-"https://www.project2025.org/wp-content/uploads/2023/02/P2025-logo_full.png", 
+"https://www.irs.gov/pub/xml_bc/53478001.gif", 
 	}
 ];
