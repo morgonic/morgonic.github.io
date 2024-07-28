872dff8d95be8947ff694264fee70ffc5d55eb75
morgonic <167047845+morgonic@users.noreply.github.com>
2024-07-27 20:38:36 -0400

    Create links5.js

---
+++ js/links5.js
@@ -0,0 +1,30 @@
+export const links = [ 
+	{ 
+		name: "WHAT IS THE NAR?", 
+		link: 
+"https://en.wikipedia.org/wiki/New_Apostolic_Reformation",
+		image: 
+"https://narandchristiannationalism.com/wp-content/uploads/2021/04/NAR-logo.png", 
+	},
+  {
+		name: "HOUSE SPEAKER INVOLVEMENT WITH NAR",
+		link:
+"https://www.npr.org/2023/12/05/1217452058/speaker-mike-johnson-draws-scrutiny-for-ties-to-far-right-christian-movements",
+    image:
+"https://narandchristiannationalism.com/wp-content/uploads/2021/04/NAR-logo.png",
+  },
+	{
+		name: "CONTACT YOUR REPRESENTATIVES FORM",
+		link:
+"https://drive.google.com/file/d/1VeeGdm54bZ8ZsEhobm2p_hu9Gtd4e211/view?usp=drive_link",
+    image:
+"https://narandchristiannationalism.com/wp-content/uploads/2021/04/NAR-logo.png",
+  },
+	{ 
+		name: "FIND YOUR REPRESENTATIVES TO CONTACT",
+		link:
+"https://www.commoncause.org/find-your-representative/",
+    image: 
+"https://narandchristiannationalism.com/wp-content/uploads/2021/04/NAR-logo.png",
+  }
+];
